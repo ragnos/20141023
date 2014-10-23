8d04aa17f37ee52a8038fc833f8c4eb2725b0975
ragnos <ragnos@utzone.de>
2014-10-23 22:02:50 +0200

    Revert "Auto - Q3 done"
    This reverts commit 1cfd2555ca9fa72bc704b8c2b6a116d33d8a6b37.

--- 20141023_auto/src/pkg20141023_auto/Main.java
+++ 20141023_auto/src/pkg20141023_auto/Main.java
@@ -15,14 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Auto newCar = new Auto("K-FR-5678", 130);
-        newCar.status();
-        newCar.beschleunige(23.4);
-        System.out.print(newCar.getGeschwindigkeit());
-        System.out.print("\n");
-        newCar.beschleunige(1337.0);
-        System.out.print(newCar.getGeschwindigkeit());
-        System.out.print("\n");
+        // TODO code application logic here
     }
     
 }
